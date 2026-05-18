1f32860151afefa34cd3eb1d4c8602cc6db7918f
nagyleventetech-ops <nagylevente.tech@gmail.com>
2026-05-18 14:15:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  root: './', // Azt mondjuk neki: itt vagyunk a gyökérben
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'
+      input: './index.html' // Itt keresd a belépési pontot
     }
   }
 })
