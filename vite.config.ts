fd874698f4b5f37ef8dc08533f47cd48c3c356a5
nagyleventetech-ops <nagylevente.tech@gmail.com>
2026-06-01 20:07:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,14 @@ export default defineConfig(({mode}) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          szallasadok: path.resolve(__dirname, 'szallasadok.html'),
+        },
+      },
+    },
     server: {
       host: '0.0.0.0',
       port: 3000,
